fb9e59579ec713c63038ab26bac0ea91f04f5beb
Minhyuk_Lee <kazesky93@gmail.com>
2022-05-26 21:14:36 +0900

    주석추가

--- src/algorithm20220524/Codeup_1284.java
+++ src/algorithm20220524/Codeup_1284.java
@@ -25,9 +25,9 @@ public class Codeup_1284 {
 		int b = 0;
 		
 		for(int i=2;i<n;i++) {
-			if(prime(i)) {
-				if(n%i==0) {
-					if(prime(n/i)){
+			if(prime(i)) {//소수면 나눠라
+				if(n%i==0) {	//그수로 나눠지니? 나눠지면?
+					if(prime(n/i)){ //몫이 소수니?
 						a = i;
 						b = n/i;
 						break;
@@ -35,7 +35,7 @@ public class Codeup_1284 {
 				}
 			}
 		}
-		if(a >= 2) {
+		if(a!=0) {//a가 0이아니면 소수(위에서 어떤 소수가 들어왔을테니)
 			System.out.println(a+" "+b);
 		}
 		else System.out.println("wrong number");
